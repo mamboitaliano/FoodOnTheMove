eefa85562e8c8e9be8d181598eab4f2b1d9a3c19
tarora2014 <tarora@g.clemson.edu>
2015-11-07 12:47:38 -0800

    added database and seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,29 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+require 'csv'
+
+csv_text = File.read(Rails.root.join('lib','trucks','Mobile_Food_Facility_Permit.csv'))
+csv = CSV.parse(csv_text, :headers =>true)
+
+csv.each do |row|
+  t = Truck.new
+
+  t.location = row['locationid']
+  t.applicant = row['Applicant']
+  t.facility_type = row['FacilityType']
+  t.location_description = row['LocationDescription']
+  t.address = row['Address']
+  t.block = row['block']
+  t.lot = row['lot']
+  t.status = row['Status']
+  t.food_items = row['FoodItems']
+  t.x = row['X']
+  t.y = row['Y']
+  t.latitude = row['Latitude']
+  t.longitude = row['Longitude']
+  t.schedule = row['Schedule']
+  t.save
+  puts "#{t.applicant} Entered!!!"
+end
