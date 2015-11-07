8e945f274007acdbf7985f8a75f7099fdbd8a473
raidersofthenight <ianemailaddress@gmail.com>
2015-11-07 15:37:56 -0800

    create and position map element

---
+++ app/assets/javascripts/map/gmap.js
@@ -0,0 +1,12 @@
+function initialize() {
+	var mapCanvas = document.getElementById('map');
+
+	var mapOptions = {
+		center: new google.maps.LatLng(37.7833, -122.4167),
+		zoom: 13,
+		mapTypeId: google.maps.MapTypeId.ROADMAP
+	}
+	var map = new google.maps.Map(mapCanvas, mapOptions);
+}
+google.maps.event.addDomListener(window, 'load', initialize);
+
