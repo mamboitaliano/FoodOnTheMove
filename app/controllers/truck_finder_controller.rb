6943c0197a32aa68115e98c6cff782a24e85a14d
tarora2014 <tarora@g.clemson.edu>
2015-11-07 16:51:33 -0800

    added forms and search for the forms

--- app/controllers/truck_finder_controller.rb
+++ app/controllers/truck_finder_controller.rb
@@ -1,5 +1,19 @@
 class TruckFinderController < ApplicationController
 	def index
-		
+    # if params
+    #   p "*" * 100
+    #   p params
+    # end
+    # @trucks = Truck.all
+    # @trucks = Truck.select("distinct applicant")
+    if (params[:truck_name])
+      @trucks = Truck.where(applicant:params[:truck_name])
+    elsif (params[:distance])
+      @trucks = Truck.all
+    elsif (params[:food_item])
+      @trucks = Truck.where('food_items LIKE ?', '%'+params[:food_item].downcase+'%').all
+    else
+      @trucks = Truck.all
+    end
 	end
 end
