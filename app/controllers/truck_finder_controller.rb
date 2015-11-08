258d263a9a3adf004fdd81116477c10a4554d70e
tarora2014 <tarora@g.clemson.edu>
2015-11-07 18:05:40 -0800

    remove unwated code

--- app/controllers/truck_finder_controller.rb
+++ app/controllers/truck_finder_controller.rb
@@ -1,11 +1,5 @@
 class TruckFinderController < ApplicationController
 	def index
-    # if params
-    #   p "*" * 100
-    #   p params
-    # end
-    # @trucks = Truck.all
-    # @trucks = Truck.select("distinct applicant")
     if (params[:truck_name])
       @trucks = Truck.where(applicant:params[:truck_name].downcase)
     elsif (params[:distance])
